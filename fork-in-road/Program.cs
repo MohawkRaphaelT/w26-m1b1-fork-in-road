9d5c644b737be022d3629d7d8983f5a1d580ce40
RaphaelT <raphael.tetreault@mohawkcollege.ca>
2026-01-20 17:46:15 -0500

    Add implementation

--- fork-in-road/Program.cs
+++ fork-in-road/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Steps 1 & 2
+Console.WriteLine("You reach a fork in the road.");
+Console.WriteLine("Say LEFT to go towards a town.");
+Console.WriteLine("Say RIGHT to go towards a mountain.");
+
+// Step 3: get user response, remember it in string variable
+string userInput;                // declare
+userInput = Console.ReadLine()!; // initialize
+
+// Step 4: check player's response for LEFT or RIGHT
+if (userInput == "LEFT")
+{
+    Console.WriteLine("You arrive at a town called...");
+}
+else if (userInput == "RIGHT")
+{
+    Console.WriteLine("You arrive at a mountain called...");
+}
+else
+{
+    Console.WriteLine("The only valid options to type are LEFT and RIGHT");
+}
+
+Console.WriteLine("THE END");
